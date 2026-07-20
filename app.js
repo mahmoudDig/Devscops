be1bc0ff2efd7262d347c7fbc1b3cbbe00746f84
mahmoudmoselhy <mahmoudokkazox2018@gmail.com>
2026-07-20 14:43:10 +0300

    Devscps v1

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "DevSecOps Demo Application"
+    });
+});
+
+app.get("/health", (req, res) => {
+    res.status(200).json({
+        status: "UP"
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
